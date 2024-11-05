08bbf862fc4d3abf33b81e4a9edcb78cb70c7adf
s u <sriya1008@gmail.com>
2024-11-05 16:41:58 -0500

    first commit

---
+++ project.py
@@ -0,0 +1,35 @@
+# https://github.com/Sriya1008/IntroToDataScienceProject
+
+
+import numpy as np
+import json
+import gzip
+import pandas as pd
+
+### Read in data
+def parse(path):
+  g = gzip.open(path, 'rb')
+  for l in g:
+    yield json.loads(l)
+    
+data = parse('All_Beauty_5.json.gz')
+print(data)
+type(data)
+
+for obj in data:
+    print(obj)
+
+
+### Read in data to pandas dataframe
+def getDF(path):
+  i = 0
+  df = {}
+  for d in parse(path):
+    df[i] = d
+    i += 1
+  return pd.DataFrame.from_dict(df, orient='index')
+
+df = getDF('All_Beauty_5.json.gz')
+
+df.dtypes
+# print(df.dtypes)
