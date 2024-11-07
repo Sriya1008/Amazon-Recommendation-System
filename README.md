7211bcf87a974dd97f27c5e4cc8e07e7946a52cb
s u <sriya1008@gmail.com>
2024-11-07 10:18:06 -0500

    Added test/train split

---
+++ README.md
@@ -0,0 +1,3 @@
+IntroToDataScienceProject
+pip install simplejson
+
--- project.py
+++ project.py
@@ -5,6 +5,8 @@ import numpy as np
 import json
 import gzip
 import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 
 ### Read in data
 def parse(path):
@@ -30,8 +32,9 @@ def getDF(path):
   return pd.DataFrame.from_dict(df, orient='index')
 
 df = getDF('All_Beauty_5.json.gz')
-print(df)
+print(df.dtypes)
 df.dtypes
+print(df.isnull().sum())
 # print(df.dtypes)
 
 #mac problems
@@ -39,3 +42,27 @@ with open('All_Beauty_5.json', 'r') as f:
   for line in f:
     review = json.loads(line.strip())
     print(review)
+
+
+#### Splitting the data 
+train, test = train_test_split(
+    df, test_size=0.2, random_state=0)
+
+print("df shape:", df.shape)
+print("train shape:", train.shape)
+print("test shape:", test.shape)
+
+
+
+# dicMac = {}
+# i = 0
+# with open('All_Beauty_5.json', 'r') as f:
+#   for line in f:
+#     review = json.loads(line.strip())
+#     dicMac[i] = line
+#     i += 1
+#     # print(review)
+# dfmac = pd.DataFrame.from_dict(dicMac, orient='index')
+# print(type(dfmac))
+# print("dfmac shape:", dfmac.shape)
+
